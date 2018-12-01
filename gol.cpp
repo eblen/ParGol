4371b1561adc74a7da03a77efccc7a5be16a20df
John Eblen <jeblen@acm.org>
2018-11-30 18:39:15 -0800

    GOL class to store GOL data and methods

--- gol.cpp
+++ gol.cpp
@@ -1,99 +1,103 @@
 #include <array>
 
-const int WORLD_SIZE = 100;
-const int NUM_GENS   = 100;
-
-using world_grid = std::array<std::array<bool, WORLD_SIZE>, WORLD_SIZE>;
-
-// Assumes input world is empty
-void init_world(world_grid &w)
+template<int WORLD_SIZE>
+class GOL
 {
-    w[49][48] = true;
-    w[49][49] = true;
-    w[49][50] = true;
-    w[49][51] = true;
-}
+    using world_grid = std::array<std::array<bool, WORLD_SIZE>, WORLD_SIZE>;
 
-void next_gen(const world_grid &old_world, world_grid &new_world)
-{
-    for (int x=0; x<WORLD_SIZE; x++)
+    public:
+    GOL()
     {
-        for (int y=0; y<WORLD_SIZE; y++)
+        for (int x=0; x<WORLD_SIZE; x++)
         {
-            // Count neighbors
-            int num_nbrs = 0;
-            int x_minus = x == 0 ? WORLD_SIZE-1 : x-1;
-            int y_minus = y == 0 ? WORLD_SIZE-1 : y-1;
-            int x_plus  = (x+1) % WORLD_SIZE;
-            int y_plus  = (y+1) % WORLD_SIZE;
-            if (old_world[x_minus][y_minus]) num_nbrs++;
-            if (old_world[x]      [y_minus]) num_nbrs++;
-            if (old_world[x_plus] [y_minus]) num_nbrs++;
-            if (old_world[x_minus][y])       num_nbrs++;
-            if (old_world[x_plus] [y])       num_nbrs++;
-            if (old_world[x_minus][y_plus])  num_nbrs++;
-            if (old_world[x]      [y_plus])  num_nbrs++;
-            if (old_world[x_plus] [y_plus])  num_nbrs++;
-
-            // Apply rules
-            switch(num_nbrs) {
-                case 2:
-                    new_world[x][y] = old_world[x][y];
-                    break;
-                case 3:
-                    new_world[x][y] = true;
-                    break;
-                default:
-                    new_world[x][y] = false;
-
+            for (int y=0; y<WORLD_SIZE; y++)
+            {
+                old_world[x][y] = (y==0) || (x % 2 == 0) ? false : true;
             }
         }
     }
-}
 
-void print_world(const world_grid &w)
-{
-    for (int x=0; x<WORLD_SIZE; x++)
+    void next_gen()
     {
-        for (int y=0; y<WORLD_SIZE; y++)
+        for (int x=0; x<WORLD_SIZE; x++)
         {
-            if (w[x][y]) printf("*");
-            else printf(" ");
-        }
-        printf("\n");
-    }
-}
+            for (int y=0; y<WORLD_SIZE; y++)
+            {
+                // Count neighbors
+                int num_nbrs = 0;
+                int x_minus = x == 0 ? WORLD_SIZE-1 : x-1;
+                int y_minus = y == 0 ? WORLD_SIZE-1 : y-1;
+                int x_plus  = (x+1) % WORLD_SIZE;
+                int y_plus  = (y+1) % WORLD_SIZE;
+                if (old_world[x_minus][y_minus]) num_nbrs++;
+                if (old_world[x]      [y_minus]) num_nbrs++;
+                if (old_world[x_plus] [y_minus]) num_nbrs++;
+                if (old_world[x_minus][y])       num_nbrs++;
+                if (old_world[x_plus] [y])       num_nbrs++;
+                if (old_world[x_minus][y_plus])  num_nbrs++;
+                if (old_world[x]      [y_plus])  num_nbrs++;
+                if (old_world[x_plus] [y_plus])  num_nbrs++;
 
-void print_sep()
+                // Apply rules
+                switch(num_nbrs) {
+                    case 2:
+                        new_world[x][y] = old_world[x][y];
+                        break;
+                    case 3:
+                        new_world[x][y] = true;
+                        break;
+                    default:
+                        new_world[x][y] = false;
+
+                }
+            }
+        }
+
+        world_grid &tmp = old_world;
+        old_world = new_world;
+        new_world = tmp;
+    }
+
+    void print()
+    {
+        for (int x=0; x<WORLD_SIZE; x++)
+        {
+            for (int y=0; y<WORLD_SIZE; y++)
+            {
+                if (old_world[x][y]) printf("*");
+                else printf(" ");
+            }
+            printf("\n");
+        }
+    }
+
+    private:
+    world_grid WORLD1;
+    world_grid WORLD2;
+    world_grid &old_world = WORLD1;
+    world_grid &new_world = WORLD2;
+};
+
+void print_sep(int length)
 {
-    for (int x=0; x<WORLD_SIZE; x++) printf("=");
+    for (int x=0; x<length; x++) printf("=");
     printf("\n");
 }
 
 int main()
 {
-    static world_grid WORLD1;
-    static world_grid WORLD2;
-    world_grid &old_world = WORLD1;
-    world_grid &new_world = WORLD2;
+    const int WORLD_SIZE = 100;
+    const int NUM_GENS   = 100;
 
-    init_world(old_world);
-    print_world(old_world);
-    print_sep();
+    GOL<WORLD_SIZE> world;
+    world.print();
+    print_sep(WORLD_SIZE);
+
     for (int gen_num = 0; gen_num < NUM_GENS; gen_num++)
     {
-        next_gen(old_world, new_world);
-        print_world(new_world);
-        print_sep();
-
-        if (gen_num % 2 != 0) {
-            old_world = WORLD1;
-            new_world = WORLD2;
-        }
-        else {
-            old_world = WORLD2;
-            new_world = WORLD1;
-        }
+        world.next_gen();
+        world.print();
+        print_sep(WORLD_SIZE);
     }
 
     return 0;
